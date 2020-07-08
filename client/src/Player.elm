2378c41e31a7494b075bfcc6804e23667a5a10e3
Bruno Gomes <brunojabs@gmail.com>
2020-07-08 20:09:15 -0300

    Refactor Room's page state
    This commit completely rewrites the Room module to split the model from
    the page itself from the room data that will be shared.
    
    Also this models a state machine by introducing the page's states
    (reflected on the `Room.Model`).
    
    This way we can have more control on when and how we deal with certain
    events / states.
    
    The pattern I'm following on the `update` function is something like
    
    (CurrentState, Msg)
    
    So it replicate what a state machine would do.
    
    The code still a little bit messy, a clean up will happen after the
    first version is up
    
    Also sneaked some layout changes in there just for fun, no harm done

---
+++ client/src/Player.elm
@@ -0,0 +1,106 @@
+module Player exposing (..)
+
+import Json.Decode as D
+import Json.Encode as E
+
+
+type State
+    = Unstarted
+    | Ended
+    | Playing
+    | Paused
+    | Buffering
+    | VideoCued
+    | Unknown
+
+
+type Msg
+    = Play
+    | Pause
+    | LoadVideo String Float
+    | GetCurrentTime
+
+
+playerStateFromString : String -> State
+playerStateFromString stateString =
+    case stateString of
+        "Unstarted" ->
+            Unstarted
+
+        "Ended" ->
+            Ended
+
+        "Playing" ->
+            Playing
+
+        "Paused" ->
+            Paused
+
+        "Buffering" ->
+            Buffering
+
+        "VideoCued" ->
+            VideoCued
+
+        _ ->
+            Unknown
+
+
+stringFromPlayerState : State -> String
+stringFromPlayerState playerState =
+    case playerState of
+        Unstarted ->
+            "Unstarted"
+
+        Ended ->
+            "Ended"
+
+        Playing ->
+            "Playing"
+
+        Paused ->
+            "Paused"
+
+        Buffering ->
+            "Buffering"
+
+        VideoCued ->
+            "VideoCued"
+
+        Unknown ->
+            "Unknown"
+
+
+playMsgToString : Msg -> String
+playMsgToString msg =
+    case msg of
+        Play ->
+            "play"
+
+        Pause ->
+            "pause"
+
+        LoadVideo _ _ ->
+            "loadVideo"
+
+        GetCurrentTime ->
+            "getCurrentTime"
+
+
+encodePlayerMsg : Msg -> E.Value
+encodePlayerMsg playerMsg =
+    case playerMsg of
+        Play ->
+            E.object [ ( "message", E.string "play" ) ]
+
+        Pause ->
+            E.object [ ( "message", E.string "pause" ) ]
+
+        GetCurrentTime ->
+            E.object [ ( "message", E.string "getCurrentTime" ) ]
+
+        LoadVideo videoID time ->
+            E.object
+                [ ( "message", E.string "loadVideo" )
+                , ( "data", E.object [ ( "videoID", E.string videoID ), ( "time", E.float time ) ] )
+                ]
