6ae4e60dbddf73066dc95bd8d5640b4210e78382
Bruno Gomes <brunojabs@gmail.com>
2020-07-09 18:01:45 -0300

    Generate a room on the Homepage
    This adds the action to generate a link to a room and redirects its to
    the room

--- client/src/LinkGenerator.elm
+++ client/src/LinkGenerator.elm
@@ -1,10 +1,10 @@
 module LinkGenerator exposing (generate)
 
 import Random
-import Random.Char exposing (ascii)
+import Random.Char exposing (english)
 import Random.String exposing (string)
 
 
 generate : (String -> msg) -> Cmd msg
 generate msg =
-    Random.generate msg (string 10 ascii)
+    Random.generate msg (string 10 english)
